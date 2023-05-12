0ecd784a5e2f087166d75cc3300b89b15f113e75
Lizeth <lgonzalezz@minsait.com>
2023-05-12 13:42:48 -0600

    cambios

--- src/test/java/com/juanma/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/juanma/calculator/CalculatorApplicationTests.java
@@ -1,16 +1,17 @@
 package com.juanma.calculator;
 package org.junit.jupiter.api
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorApplicationTests {
+
     @Test
     public void contextLoads() {
     }
+
 }

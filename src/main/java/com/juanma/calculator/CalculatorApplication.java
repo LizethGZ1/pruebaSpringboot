86c923082557709913e6e74bdea5de64f2e26fe5
Lizeth <lgonzalezz@minsait.com>
2023-05-12 11:47:08 -0600

    Cambio

--- src/main/java/com/juanma/calculator/CalculatorApplication.java
+++ src/main/java/com/juanma/calculator/CalculatorApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
-	// Un cambio Otro cambio
-	public static void main(String[] args) {
+	public static void main(String[] args)
 		SpringApplication.run(CalculatorApplication.class, args);
-	}
-
 }

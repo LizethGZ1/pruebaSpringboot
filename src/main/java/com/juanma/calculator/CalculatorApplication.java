26481277c5b4040018bcd30fd9bdbd1a4f74e575
Lizeth <lgonzalezz@minsait.com>
2023-05-12 13:20:48 -0600

    cambios

--- src/main/java/com/juanma/calculator/CalculatorApplication.java
+++ src/main/java/com/juanma/calculator/CalculatorApplication.java
@@ -1,6 +1,6 @@
 package com.juanma.calculator;
 
-import com.termometer.example.util.Converter;
+import com.juanma.calculator.util.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
